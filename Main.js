30794338daac04437fd00d5f49c1626648422c8b
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-10-08 20:57:57 +0800

    Update Main.js
    added event listeners for the sidebar

--- Main.js
+++ Main.js
@@ -1,1 +1,17 @@
+function openNav() {
+
+	console.log("Opening Sidebar.")
+	document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  
+
+}
+
+function closeNav() {
+
+	console.log("Closing Sidebar.")
+	document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+		
+}
 
